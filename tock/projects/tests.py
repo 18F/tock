dc637963adb4f080991c678ec625e41a12f5fcb3
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-03-20 15:04:11 -0500

    added tests for projects module

--- tock/projects/tests.py
+++ tock/projects/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from projects.models import Agency, Project
 
-# Create your tests here.
+class AgencyTests(SimpleTestCase):
+
+    def test_agency_save(self):
+        agency = Agency(name='General Services Administration')
+        agency.save()
+
+        retrieved = Agency.objects.get(pk=agency.pk)
+        self.assertEqual(
+            'General Services Administration', str(retrieved))
+
+class ProjectTest(SimpleTestCase):
+    def test_project_save(self):
+        agency = Agency(name='General Services Administration')
+        agency.save()
+
+        project = Project(
+            agency=agency,
+            name="Test Project"
+        )
+        project.save()
+
+        retrieved = Project.objects.get(pk=project.pk)
+        self.assertEqual(retrieved.name, "Test Project")
