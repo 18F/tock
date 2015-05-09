fcb184581b3ea31688d57ac01f6ead1ef281ff0c
Sean Herron <sean.herron@gsa.gov>
2015-05-08 19:21:11 -0500

    convert users hotfix

--- tock/employees/management/commands/convert_users.py
+++ tock/employees/management/commands/convert_users.py
@@ -11,7 +11,5 @@ class Command(BaseCommand):
         	employee, created = Employee.objects.get_or_create(email=item.username)
         	employee.first_name = item.first_name
         	employee.last_name = item.last_name
-        	employee.start_date = item.user_data.start_date
-        	employee.end_date = item.user_data.end_date
         	employee.is_admin = item.is_staff
         	employee.save()
