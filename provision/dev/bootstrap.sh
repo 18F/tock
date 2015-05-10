53adf57905bef5918ac6729d41e1ed1052af98dd
Sean Herron <sean.herron@gsa.gov>
2015-05-09 19:22:05 -0700

    switch to postgres

--- provision/dev/bootstrap.sh
+++ provision/dev/bootstrap.sh
@@ -17,6 +17,27 @@ sudo apt-get install libbz2-dev -y
 sudo apt-get install libsqlite3-dev -y
 sudo apt-get install libreadline-dev -y
 
+if [ ! -f /home/vagrant/provision/postgres ]; then
+
+  echo 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+
+  wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+
+  sudo apt-get update
+  sudo apt-get install -y \
+    postgresql \
+    libpq-dev
+
+  sudo -u postgres psql -c "create user tock with password 'tock';"
+  sudo -u postgres psql -c "alter user tock with superuser;"
+  sudo -u postgres psql -c "CREATE DATABASE tock;"
+  sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE tock TO tock;"
+
+  su -c "touch /home/vagrant/provision/dev/postgres" vagrant
+
+fi
+
+
 sudo su vagrant <<'EOF'
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
@@ -32,6 +53,4 @@ pyenv activate tock
 pip install -r /vagrant/requirements.txt
 pip install -r /vagrant/requirements-dev.txt
 cd /vagrant/tock
-python manage.py migrate
-python manage.py loaddata dev_user projects
 EOF
