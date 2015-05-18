4e2b5819a149252af735d7d89a6bd1d19efe9f4a
Sean Herron <sean.herron@gsa.gov>
2015-05-18 15:46:53 -0400

    working bluegreen

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
+
+app_name="tock"
 
 timestamp="$(date +"%s")"
-app_name="tock"
 current_apps="$(cf apps)"
-current_vars="$(cf env $app_name)"
 
 deploy()
 {
@@ -12,8 +14,9 @@ deploy()
 
 	current_vars="$(cf env $current_deployment)"
 
-	database_url=$(echo $current_vars | sed -n "s/DATABASE_URL: \([a-zA-Z0-9:/@.-]*\)/\1/p")
-	django_secret_key=$(echo $current_vars | sed -n "s/DJANGO_SECRET_KEY: \([a-zA-Z0-9:/@.-]*\)/\1/p")
+	database_url="$(echo ${current_vars#*DATABASE_URL: } | awk '{print $1}')"
+	django_secret_key="$(echo ${current_vars#*DJANGO_SECRET_KEY: } | awk '{print $1}')"
+
 
 	echo "$current_deployment is currently deployed, pushing $next_deployment"
 	cf push $next_deployment --no-start -n $app_name-$timestamp
@@ -23,21 +26,24 @@ deploy()
 	echo "Mapping $next_deployment to the Main Domain"
 	cf map-route $next_deployment 18f.gov -n $app_name
 	cf map-route $next_deployment cf.18f.us -n $app_name
-	echo "Removing Green From the Main Domain"
+	echo "Removing $current_deployment From the Main Domain"
 	cf unmap-route $current_deployment 18f.gov -n $app_name
 	cf unmap-route $current_deployment cf.18f.us -n $app_name
-	echo "Deleting $current_deployment"
-	cf delete $current_deployment
+	read -p "Check your app. Is it functioning properly? (y/n)" -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+    	cf delete $current_deployment -f
+	else
+		deploy $next_deployment $current_deployment
+	fi
 }
 
-deploy blue green
-
-#if [ "${ $current_apps/green }" = "$current_apps" ]; then
-#	#deploy green blue
-#	echo "green deployed"
-#elif [ "${ $current_apps/blue }" = "$current_apps" ]; then
-#	#deploy blue green
-#	echo "blue deployed"
-#else
-#	echo "No Current Blue/Green Deployment Found!"
-#fi
+if [[ $current_apps == *"green"* ]]; then
+	echo "Green Exists, Deploying Blue"
+	deploy green blue
+elif [[ $current_apps == *"blue"* ]]; then
+	echo "Blue Exists, Deploying Green"
+	deploy blue green
+else
+	echo "No existing blue or green app, please create one first!"
+fi
