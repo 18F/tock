293f70cd33521abd4cc29a9bcf1c01a029470a24
Shawn Allen <shawn.allen@gsa.gov>
2015-05-29 10:43:49 -0700

    add project_timeline.csv endpoint and view

--- tock/api/views.py
+++ tock/api/views.py
@@ -8,9 +8,12 @@ from django.contrib.auth.models import User, Group
 from hours.models import ReportingPeriod, Timecard, TimecardObject
 from projects.models import Project, Agency, AccountingCode
 
-from rest_framework import serializers
-from rest_framework import generics
-from rest_framework import pagination
+from rest_framework import serializers, generics, pagination, renderers
+
+from rest_framework.renderers import JSONRenderer
+from .renderers import PaginatedCSVRenderer, StreamingCSVRenderer
+
+import csv
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
     page_size = 100
@@ -38,6 +41,7 @@ class TimecardSerializer(serializers.ModelSerializer):
         model = TimecardObject
         fields = ('user', 'project', 'start_date', 'end_date', 'hours_spent', 'billable',)
 
+
 class ProjectList(generics.ListAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
@@ -49,38 +53,75 @@ class UserList(generics.ListAPIView):
     pagination_class = StandardResultsSetPagination
 
 class TimecardList(generics.ListAPIView):
+    queryset = TimecardObject.objects.order_by('timecard__reporting_period__start_date')
+
     serializer_class = TimecardSerializer
     pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
-        params = self.request.QUERY_PARAMS
-        queryset = TimecardObject.objects.order_by('timecard__reporting_period__start_date')
+        return get_timecards(self.queryset, self.request.QUERY_PARAMS)
 
-        # if the `date` query string parameter (in YYYY-MM-DD format) is
-        # provided, get rows for which the date falls within their reporting
-        # date range
-        if 'date' in params:
-            reporting_date = params.get('date')
-            # TODO: validate YYYY-MM-DD format
-            queryset = queryset.filter(
-                timecard__reporting_period__start_date__lte=reporting_date,
-                timecard__reporting_period__end_date__gte=reporting_date
-            )
+def ProjectTimelineView(request):
+    queryset = get_timecards(TimecardList.queryset, request.GET)
 
-        if 'user' in params:
-            # allow either user name or ID
-            user = params.get('user')
-            if user.isnumeric():
-                queryset = queryset.filter(timecard__user__id=user)
-            else:
-                queryset = queryset.filter(timecard__user__username=user)
+    data = queryset.values(
+        'project__id',
+        'project__name',
+        # 'timecard__user__username',
+        'timecard__reporting_period__start_date',
+        # 'timecard__reporting_period__end_date',
+        'project__accounting_code__billable'
+    ).annotate(hours_spent=Sum('hours_spent'))
 
-        if 'project' in params:
-            # allow either project name or ID
-            project = params.get('project')
-            if project.isnumeric():
-                queryset = queryset.filter(project__id=project)
-            else:
-                queryset = queryset.filter(project__name=project)
+    fields = (
+        ('project_id', 'project__id'),
+        ('project_name', 'project__name'),
+        ('start_date', 'timecard__reporting_period__start_date'),
+        ('billable', 'project__accounting_code__billable'),
+        ('hours_spent', 'hours_spent'),
+    )
 
-        return queryset
+    def map_row(row):
+        return dict(
+            (dest, row.get(src)) for (dest, src) in fields
+        )
+
+    data = map(map_row, data)
+
+    response = HttpResponse(content_type='text/csv')
+    fieldnames = [f[0] for f in fields]
+    writer = csv.DictWriter(response, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
+    return response
+
+def get_timecards(queryset, params={}):
+    # if the `date` query string parameter (in YYYY-MM-DD format) is
+    # provided, get rows for which the date falls within their reporting
+    # date range
+    if 'date' in params:
+        reporting_date = params.get('date')
+        # TODO: validate YYYY-MM-DD format
+        queryset = queryset.filter(
+            timecard__reporting_period__start_date__lte=reporting_date,
+            timecard__reporting_period__end_date__gte=reporting_date
+        )
+
+    if 'user' in params:
+        # allow either user name or ID
+        user = params.get('user')
+        if user.isnumeric():
+            queryset = queryset.filter(timecard__user__id=user)
+        else:
+            queryset = queryset.filter(timecard__user__username=user)
+
+    if 'project' in params:
+        # allow either project name or ID
+        project = params.get('project')
+        if project.isnumeric():
+            queryset = queryset.filter(project__id=project)
+        else:
+            queryset = queryset.filter(project__name=project)
+
+    return queryset
