82c2dbeeb9e0da57962df7a6d4a74d08a1776b19
Shawn Allen <shawn.allen@gsa.gov>
2015-06-01 14:30:44 -0700

    remove redundant settings from test.py

--- tock/tock/settings/test.py
+++ tock/tock/settings/test.py
@@ -11,16 +11,4 @@ DATABASES = {
     }
 }
 
-INSTALLED_APPS += ('django.contrib.admin', 'django.contrib.auth',
-                   'django.contrib.sessions', 'django.contrib.messages',)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',)
-
 MEDIA_ROOT = './media/'
