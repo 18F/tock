38a891ac3cdf86a2ccde36db08de68a0bf391ae3
Joshua Carp <jm.carp@gmail.com>
2015-07-07 17:12:58 -0400

    Start coverage before tests.
    Django 1.7+ imports models before starting the testing process, so
    models appear to never be tested. This patch uses a workaround described
    in https://github.com/django-nose/django-nose/issues/180.
    
    [Resolves #198]

--- tock/manage.py
+++ tock/manage.py
@@ -2,9 +2,23 @@
 import os
 import sys
 
-if __name__ == "__main__":
-  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tock.settings.dev")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tock.settings.dev')
+    from django.core.management import execute_from_command_line
+    testing = 'test' in sys.argv
 
-  from django.core.management import execute_from_command_line
+    if testing:
+        import coverage
+        cov = coverage.coverage(
+            source=['tock', 'employees', 'projects', 'hours', 'api'],
+            omit=['*/tests*', '*/migrations/*', '*/settings/*'],
+        )
+        cov.erase()
+        cov.start()
 
-  execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
+
+    if testing:
+        cov.stop()
+        cov.save()
+        cov.report()
