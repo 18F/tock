6dae6a6f4353084e1e9f4d9ffe71c94ec8f1af8a
ramirezg <gabriel.ramirez@gsa.gov>
2015-07-10 10:52:22 -0400

    updating test to cover models and model methods

--- tock/projects/tests.py
+++ tock/projects/tests.py
@@ -4,25 +4,40 @@ from projects.models import Agency, Project, AccountingCode
 
 class AgencyTests(SimpleTestCase):
 
-  def test_agency_save(self):
-    agency = Agency(name='General Services Administration')
-    agency.save()
+    def test_agency_save(self):
+        """ Test that agency model works correctly """
+        agency = Agency(name='General Services Administration')
+        agency.save()
+        retrieved = Agency.objects.get(pk=agency.pk)
+        self.assertEqual('General Services Administration', str(retrieved))
 
-    retrieved = Agency.objects.get(pk=agency.pk)
-    self.assertEqual('General Services Administration', str(retrieved))
 
+class ProjectAndAccountingCodeTest(SimpleTestCase):
 
-class ProjectTest(SimpleTestCase):
+    def setUp(self):
+        agency = Agency(name='General Services Administration')
+        agency.save()
+        accounting_code = AccountingCode(
+            code='abc', agency=agency, office='18F', billable=True)
+        accounting_code.save()
+        self.project = Project(accounting_code=accounting_code,
+                               name="Test Project")
+        self.project.save()
 
-  def test_project_save(self):
-    agency = Agency(name='General Services Administration')
-    agency.save()
+    def test_model(self):
+        """ Check that the project model stores data correctly and links to
+        AccountingCode model properly"""
+        retrieved = Project.objects.get(pk=self.project.pk)
+        self.assertEqual(
+            retrieved.accounting_code.agency.name,
+            'General Services Administration')
+        self.assertEqual(retrieved.accounting_code.office, '18F')
+        self.assertTrue(retrieved.accounting_code.billable)
 
-    accounting_code = AccountingCode(agency=agency)
-    accounting_code.save()
-
-    project = Project(accounting_code=accounting_code, name="Test Project")
-    project.save()
-
-    retrieved = Project.objects.get(pk=project.pk)
-    self.assertEqual(retrieved.name, "Test Project")
+    def test_is_billable(self):
+        """ Check that the is_billable method works """
+        retrieved = Project.objects.get(name='Test Project')
+        self.assertTrue(retrieved.is_billable())
+        retrieved.accounting_code.billable = False
+        retrieved.accounting_code.save()
+        self.assertFalse(retrieved.is_billable())
