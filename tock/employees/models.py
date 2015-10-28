0ee649b8b0a6148325df7a8e98082dc0603fd85e
Patrick Bateman <batemapf@gmail.com>
2015-10-27 21:21:28 -0400

    Update models.py

--- tock/employees/models.py
+++ tock/employees/models.py
@@ -6,4 +6,3 @@ class UserData(models.Model):
     user = models.OneToOneField(User, related_name='user_data')
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    division = models.CharField(max_length=200, blank=True)
