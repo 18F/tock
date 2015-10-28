2a212bb869e67e2cd1ae62fc0b5dd28eeb6eaffd
Patrick Bateman <batemapf@gmail.com>
2015-10-27 21:12:55 -0400

    Update models.py
    add division attribute to user model

--- tock/employees/models.py
+++ tock/employees/models.py
@@ -6,3 +6,4 @@ class UserData(models.Model):
     user = models.OneToOneField(User, related_name='user_data')
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
+    division = models.CharField(max_length=200, blank=True)
