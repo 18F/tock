7881145a91b70772098d183e2f1a6727bf6fbce4
Patrick Bateman <patrick.bateman@gsa.gov>
2015-11-03 15:52:36 -0500

    finally got migrations right

---
+++ tock/projects/migrations/0004_remove_project_priority.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0003_auto_20151103_2041'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='project',
+            name='priority',
+        ),
+    ]
