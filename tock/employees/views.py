9c4714caacc018487b759032c52fcea55d085da3
Dan Siddoway <dan@ds1.co>
2016-03-04 09:59:47 -0800

    wrote a FormView and its associated HTML template, and added its URL route
    The form template is an extension of the `base.html` template, and may be accessed at `/employees/<username>/travel_request` provided that the currently logged-in user is either `<username>` or a superuser.  (I also added this link to the `user_form.html` template for the sake of convenience.)
    
    The form template employs [CSRF protection](https://docs.djangoproject.com/en/1.9/ref/csrf/#how-to-use-it).
    
    The `requestor_name` and `requestor_email` fields are automatically populated with the full name and email address, respectively, of the user corresponding to `<username>`.
    
    The form template uses the jQuery Datepicker widget, for the sake of [consistency](https://github.com/18F/tock/blob/master/tock/tock/templates/employees/user_form.html#L29).

--- tock/employees/views.py
+++ tock/employees/views.py
@@ -1,14 +1,17 @@
 import datetime
+from string import Template
 
 from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
 from django.core.urlresolvers import reverse
+from django.template import Context
+from django.template.loader import render_to_string
 from django.views.generic import ListView
 from django.views.generic.edit import FormView
 
-from tock.utils import PermissionMixin, IsSuperUserOrSelf
-
-from .forms import UserForm
+from .forms import UserForm, UserTravelRequestForm
 from .models import UserData
+from tock.utils import PermissionMixin, IsSuperUserOrSelf
 
 
 def parse_date(date):
@@ -70,4 +73,69 @@ class UserFormView(PermissionMixin, FormView):
         return reverse(
             'employees:UserListView',
             current_app=self.request.resolver_match.namespace,
+        )
+
+
+class UserTravelRequestFormView(PermissionMixin, FormView):
+    form_class = UserTravelRequestForm
+    template_name = 'employees/user_travel_form.html'
+    permission_classes = (IsSuperUserOrSelf,)
+
+    def get_context_data(self, **kwargs):
+        kwargs['username'] = self.kwargs['username']
+        return super(UserTravelRequestFormView, self).get_context_data(**kwargs)
+
+    def get_initial(self):
+        initial = super(UserTravelRequestFormView, self).get_initial()
+        user, created = User.objects.get_or_create(
+            username=self.kwargs['username']
+        )
+        initial['requestor_email'] = user.email
+        initial['requestor_name'] = user.first_name + ' ' + user.last_name
+
+        return initial
+
+    def form_valid(self, form):
+        if form.is_valid():
+            subject = Template(
+                '${billability} - ${tock_project_name} - #${tock_project_id} -- ${departure_date} - ${return_date}'
+            ).substitute(
+                billability=form.cleaned_data['billability'],
+                tock_project_name=form.cleaned_data['tock_project_name'],
+                tock_project_id=form.cleaned_data['tock_project_id'],
+                departure_date=form.cleaned_data['departure_date'],
+                return_date=form.cleaned_data['return_date'],
+            )
+            message_context = Context({
+                'home_location': form.cleaned_data['home_location'],
+                'work_location': form.cleaned_data['work_location'],
+                'departure_date': form.cleaned_data['departure_date'],
+                'return_date': form.cleaned_data['return_date'],
+                'work_to_be_done': form.cleaned_data['work_to_be_done'],
+                'first_day_of_travel_work_date': form.cleaned_data['first_day_of_travel_work_date'],
+                'requestor_name': form.cleaned_data['requestor_name']
+            })
+            message_plain = render_to_string('employees/user_travel_form_email.txt', message_context)
+            message_html = render_to_string('employees/user_travel_form_email.html', message_context)
+
+            email = EmailMultiAlternatives(
+                to=[
+                    form.cleaned_data['client_email']
+                ],
+                subject=subject,
+                body=message_plain,
+                reply_to=[
+                    form.cleaned_data['requestor_email'],
+                    '18fTravelAuths@gsa.gov'
+                ]
+            )
+            email.attach_alternative(message_html, 'text/html')
+            email.send()
+
+        return super(UserTravelRequestFormView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse(
+            'employees:UserListView',
+            current_app=self.request.resolver_match.namespace
         )
