e37b15ac7e36cdb03321c4e3b4718c1aedf17f1c
Patrick Bateman <patrickbateman@C02QQ0MZFVH8.local>
2016-08-26 10:31:10 -0400

    add search to float task admin

--- tock/float/admin.py
+++ tock/float/admin.py
@@ -3,5 +3,6 @@ from .models import FloatTasks
 
 class FloatTasksAdmin(admin.ModelAdmin):
     readonly_fields = ('tock_pk',)
+    search_fields = ('im', 'project_name')
 
 admin.site.register(FloatTasks, FloatTasksAdmin)
