09f08b504cf8f2b1c28a92862070023331cd93d0
Patrick Bateman <patrickbateman@C02QQ0MZFVH8.fios-router.home>
2016-09-27 22:12:01 -0400

    roll back unrelated changes

--- tock/employees/models.py
+++ tock/employees/models.py
@@ -50,41 +50,6 @@ class UserData(models.Model):
     def get_supervisees(self):
         return UserData.objects.filter(supervisor=self.user)
 
-    def get_timecard_objects(self):
-        tos = TimecardObject.objects.filter(timecard__user=self.user, timecard__submitted=True)
-        return tos
-
-    def get_lifetime_utilization(self):
-        tos = self.get_timecard_objects()
-        all_hours = tos.all().aggregate(
-            Sum('hours_spent'))['hours_spent__sum']
-        if all_hours:
-            billable_hours = tos.all().filter(
-                project__accounting_code__billable=True).all().aggregate(
-                Sum('hours_spent'))['hours_spent__sum']
-            utilization = '{:.3}%'.format((billable_hours / all_hours)*100)
-            return utilization
-        else:
-            return 'No hours recorded.'
-
-    def get_last_weeks_utlization(self):
-        date = datetime.date.today()
-        rp = ReportingPeriod.objects.filter(
-            end_date__lte=date).latest(
-            'timecard__reporting_period__start_date').start_date
-        tos = self.get_timecard_objects().all().filter(
-            timecard__reporting_period__start_date=rp)
-        all_hours = tos.all().aggregate(
-            Sum('hours_spent'))['hours_spent__sum']
-        if all_hours:
-            billable_hours = tos.all().filter(
-                project__accounting_code__billable=True).all().aggregate(
-                Sum('hours_spent'))['hours_spent__sum']
-            lst_wk_utilization = '{:.3}%'.format((billable_hours / all_hours)*100)
-            return lst_wk_utilization
-        else:
-            return 'No hours recorded'
-
     def save(self, *args, **kwargs):
         if self.current_employee is False:
             try:
