2e639ebe0050cf7eb3ec7911e1d1ca9e7c163142
Patrick Bateman <patrickbateman@C02QQ0MZFVH8.local>
2016-10-21 17:39:35 -0400

    when the tests pass locally on a friday, you go home

--- tock/utilization/views.py
+++ tock/utilization/views.py
@@ -19,7 +19,14 @@ class GroupUtilizationView(ListView):
         """Although recent_rps is set to last four reporting periods, could
         accept a form response that allows the user or app to dynamically
         customize number of periods to include in the queryset."""
-        recent_rps = get_dates(4)
+
+        available_periods = len(ReportingPeriod.objects.all())
+        requested_periods = 4
+
+        if available_periods >= requested_periods:
+            recent_rps = get_dates(requested_periods)
+        else:
+            recent_rps = get_dates(available_periods)
 
         billable_staff = User.objects.filter(
             user_data__is_billable=True,
@@ -35,7 +42,7 @@ class GroupUtilizationView(ListView):
             max_tos = TimecardObject.objects.filter(
                 timecard__user=staffer,
                 submitted=True,
-                timecard__reporting_period__start_date__gte=self.recent_rps[3]
+                timecard__reporting_period__start_date__gte=recent_rps[3]
             ).prefetch_related(
                 'timecard__user'
             ).prefetch_related(
@@ -47,7 +54,7 @@ class GroupUtilizationView(ListView):
             year. This operation is unnecessary except at the beginning of the
             fiscal year."""
             fytd_tos = max_tos.filter(
-                timecard__reporting_period__start_date__gte=self.recent_rps[2])
+                timecard__reporting_period__start_date__gte=recent_rps[2])
 
             """Calcuates the billable hours decimal value in the queryset."""
             fytd_billable_hours = fytd_tos.filter(
@@ -71,7 +78,7 @@ class GroupUtilizationView(ListView):
             function."""
 
             recent_tos = max_tos.filter(
-                timecard__reporting_period__start_date__gte=self.recent_rps[1]
+                timecard__reporting_period__start_date__gte=recent_rps[1]
             )
 
             """Calcuates the billable hours decimal value in the queryset."""
@@ -95,7 +102,7 @@ class GroupUtilizationView(ListView):
             period.
             """
             last_tos = recent_tos.filter(
-                timecard__reporting_period__end_date=self.recent_rps[0]
+                timecard__reporting_period__end_date=recent_rps[0]
             )
 
             """Calcuates the billable hours decimal value in the queryset."""
@@ -134,7 +141,15 @@ class GroupUtilizationView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(GroupUtilizationView, self).get_context_data(**kwargs)
-        recent_rps = get_dates(4)
+
+        available_periods = len(ReportingPeriod.objects.all())
+        requested_periods = 4
+
+        if available_periods >= requested_periods:
+            recent_rps = get_dates(requested_periods)
+        else:
+            recent_rps = get_dates(available_periods)
+
         context.update(
             {
                 'unit_choices': UserData.UNIT_CHOICES,
