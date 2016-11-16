bb9cdd4399fb30b7e02f84375218670437766c01
Patrick Bateman <patrickbateman@C02QQ0MZFVH8.local>
2016-11-16 14:02:28 -0500

    update project admin panel and add validation

--- tock/projects/admin.py
+++ tock/projects/admin.py
@@ -15,7 +15,7 @@ class AccountingCodeAdmin(admin.ModelAdmin):
 class ProfitLossAccountForm(forms.ModelForm):
     model = ProfitLossAccount
     def clean(self):
-
+        print(self.cleaned_data)
         if self.cleaned_data.get(
             'as_start_date'
         ) > self.cleaned_data.get(
@@ -31,8 +31,39 @@ class ProfitLossAccountAdmin(admin.ModelAdmin):
     search_fields = ['name',]
     readonly_fields = ['as_active']
 
+class ProjectForm(forms.ModelForm):
+    model = Project
+    def clean(self):
+        print(self.cleaned_data)
+        project_sd = self.cleaned_data['start_date']
+        pl_acct_sd = self.cleaned_data['profit_loss_account'].as_start_date
+        if project_sd and (project_sd < pl_acct_sd):
+            raise forms.ValidationError(
+                'Profit/Loss accounting string will not be active until {}. '\
+                'Please select an profit/loss accounting string that is '\
+                'will be active on or before the start date of this project.'\
+                .format(pl_acct_sd)
+            )
+        return self.cleaned_data
+
 class ProjectAdmin(admin.ModelAdmin):
+    form = ProjectForm
     search_fields = ['name',]
+    fields = [
+        'name',
+        'mbnumber',
+        'accounting_code',
+        'profit_loss_account',
+        'project_lead',
+        'start_date',
+        'end_date',
+        'active',
+        'agreement_URL',
+        'description',
+        'alerts',
+        'notes_required',
+        'notes_displayed'
+    ]
 
 class ProjectAlertAdmin(admin.ModelAdmin):
     search_fields = ['title',]
