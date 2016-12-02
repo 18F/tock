03cd1b2a57cb45463fd9a975f14f700ba00c4d62
Gonzalo Maldonado <elg0nz@gmail.com>
2016-12-01 17:39:35 -0800

    Fix bug while comparing dates on TimeCardTests
    Per the Python docs
    https://docs.python.org/2/library/datetime.html#datetime.datetime.today
    
    datetime.datetime.today() returns current local datetime with tzinfo
    None.
    
    Since the timecard objects during testing are stored on UTC time
    (according to my local tests), the tests fail when the datetime.date
    drifts enough from utc to return a different day.
    
    This updates the tests to use datetime.datetime.utcnow().day instead,
    which prevents this issue.
    
    We also recommend evaluating  https://github.com/spulec/freezegun to make
    these date comparisons deterministic.

--- tock/hours/tests/test_models.py
+++ tock/hours/tests/test_models.py
@@ -86,8 +86,9 @@ class TimecardTests(TestCase):
         timecard = Timecard.objects.first()
         self.assertEqual(timecard.user.pk, 1)
         self.assertEqual(timecard.reporting_period.exact_working_hours, 40)
-        self.assertEqual(timecard.created.day, datetime.date.today().day)
-        self.assertEqual(timecard.modified.day, datetime.date.today().day)
+        today_utc_day = datetime.datetime.utcnow().day
+        self.assertEqual(timecard.created.day, today_utc_day)
+        self.assertEqual(timecard.modified.day, today_utc_day)
         self.assertEqual(len(timecard.time_spent.all()), 2)
 
     def test_time_card_unique_constraint(self):
@@ -112,8 +113,10 @@ class TimecardTests(TestCase):
         self.assertEqual(timecardobj.timecard.user.pk, 1)
         self.assertEqual(timecardobj.project.name, 'openFEC')
         self.assertEqual(timecardobj.hours_spent, 12)
-        self.assertEqual(timecardobj.created.day, datetime.date.today().day)
-        self.assertEqual(timecardobj.modified.day, datetime.date.today().day)
+        today_utc_day = datetime.datetime.utcnow().day
+
+        self.assertEqual(timecardobj.created.day, today_utc_day)
+        self.assertEqual(timecardobj.modified.day, today_utc_day)
 
     def test_timecardobject_hours(self):
         """Test the TimeCardObject hours method."""
