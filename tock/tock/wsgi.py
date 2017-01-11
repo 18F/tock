9ebc89a678633f701ad19fa1be0fed3c0f88e124
Annalee Flower Horne <anna.flowerhorne@gsa.gov>
2017-01-11 18:08:09 -0500

    resolving a namespace error

--- tock/tock/wsgi.py
+++ tock/tock/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-import newrelic
+import newrelicinit
 newrelic.initialize()
 
 import os
