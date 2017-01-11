fa695ef1d663e5e7e19ae592d7bf741ba79a77c5
Annalee Flower Horne <anna.flowerhorne@gsa.gov>
2017-01-11 18:41:20 -0500

    import problems fixed for real this time

--- tock/tock/wsgi.py
+++ tock/tock/wsgi.py
@@ -6,8 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-from tock.tock import newrelicinit
-newrelic.initialize()
+from tock import newrelicinit
+newrelicinit.initialize()
 
 import os
 
