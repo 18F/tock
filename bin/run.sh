7c3bb851ccaa2c992acab39fd584ed92fac0f7a1
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2018-01-23 09:52:51 -0500

    Update run script to be executable and fix paths

--- bin/run.sh
+++ bin/run.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 
-cd ../tock
+cd tock
 python manage.py migrate --settings=tock.settings.production --noinput
 python manage.py collectstatic --settings=tock.settings.production --noinput
 gunicorn -k gevent -w 2 tock.wsgi:application
