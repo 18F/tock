c048998dff089ac9e576cdd35a6d3677ec4a5b7a
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2018-01-24 16:52:07 -0500

    Only migrate on 0th index

--- bin/run.sh
+++ bin/run.sh
@@ -4,6 +4,12 @@ set -o errexit
 set -o pipefail
 
 cd tock
-python manage.py migrate --settings=tock.settings.production --noinput
+
+# Only run migrations on the zeroth index when in a cloud.gov environment
+if [[ -v CF_INSTANCE_INDEX && $CF_INSTANCE_INDEX == 0 ]]
+then
+  python manage.py migrate --settings=tock.settings.production --noinput
+fi
+
 python manage.py collectstatic --settings=tock.settings.production --noinput
 gunicorn -k gevent -w 2 tock.wsgi:application
