06ffe743673f32188813e48217a75e1bdf154953
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2018-02-08 11:05:20 -0500

    Explain why there's a regular expression here

--- tock/tock/context_processors.py
+++ tock/tock/context_processors.py
@@ -9,6 +9,9 @@ def version_url(request):
         'x_tock_release_url': '%stree/%s' % (base_url, name),
         'x_tock_release_name': name,
     }
+
+    # Regular expression here matches the regular expression found in
+    # .circleci/config.yml for filtering on tags for Production deployments
     if re.match('/v20[1-9][0-9][0-9]+\.[0-9]+/', name):
         response['release_url'] = '%sreleases/tag/%s' % (base_url, name)
 
