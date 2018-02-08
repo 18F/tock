f60a02c7752de2ce7ac2eaff799a677805975219
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2018-02-08 11:04:58 -0500

    Rename keys to avoid name collisions;
    h/t @tbaxter-18f

--- tock/tock/context_processors.py
+++ tock/tock/context_processors.py
@@ -6,8 +6,8 @@ def version_url(request):
     base_url = 'https://github.com/18F/tock/'
     name = settings.VERSION
     response = {
-        'release_url': '%stree/%s' % (base_url, name),
-        'release_name': settings.VERSION,
+        'x_tock_release_url': '%stree/%s' % (base_url, name),
+        'x_tock_release_name': name,
     }
     if re.match('/v20[1-9][0-9][0-9]+\.[0-9]+/', name):
         response['release_url'] = '%sreleases/tag/%s' % (base_url, name)
