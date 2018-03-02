c67e399bb4921e15c70587b28ebd2d52fdd3f5be
Atul Varma <varmaa@gmail.com>
2018-03-02 12:42:19 -0500

    Convert hours API endpoints to be DRF API views.

--- tock/tock/settings/base.py
+++ tock/tock/settings/base.py
@@ -100,6 +100,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
