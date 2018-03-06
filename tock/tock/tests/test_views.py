1b12f2ef7501c970d356a7726eb4c5a1c04308d4
Atul Varma <varmaa@gmail.com>
2018-03-06 15:05:58 -0500

    Add logout view and menu item. Fixes #731.

---
+++ tock/tock/tests/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class ViewsTests(TestCase):
+    def test_logout_logs_user_out(self):
+        user = User.objects.create_user(username='foo')
+        self.client.force_login(user)
+
+        response = self.client.get('/logout')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_authenticated())
