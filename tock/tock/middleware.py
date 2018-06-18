68f9abb6f58419676527c58983d31f6a4dcdb9b7
Roger Steve Ruiz <hi@rog.gr>
2018-06-18 13:06:51 -0400

    Add Auto Logout functionality (#815)
    * Add Auto Logout functionality
    
    * Update middleware to the latest & greatest
    
    * Upgrade settings for MIDDLEWARE
    
    * Update order of imports
    
    * Fix test check for tock session activity data
    
    * Move the fmt up

---
+++ tock/tock/middleware.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.contrib import auth
+
+
+class AutoLogout(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        fmt = '%Y%m%d%H%M%S'
+
+        # Check if user exists and is logged in
+        if request.user and request.user.is_authenticated():
+
+            logout_time_in_seconds = settings.AUTO_LOGOUT_DELAY_MINUTES * 60
+
+            # Compare the time of the last activity with the logout delay
+            try:
+                session_time = datetime.strptime(
+                    request.session['tock_last_activity'],
+                    fmt
+                )
+                if datetime.now() - session_time > \
+                   timedelta(seconds=logout_time_in_seconds):
+                    auth.logout(request)
+                    del request.session['tock_last_activity']
+                    return self.get_response(request)
+            except KeyError:
+                pass
+
+        request.session['tock_last_activity'] = \
+            datetime.now().strftime(fmt)
+
+        return self.get_response(request)
