aeb4a7ecc36f4bd0ccfbca0a97c519d66a304da2
Joe Krzystan <joseph.krzystan@gsa.gov>
2019-03-05 10:13:58 -0500

    Use default Django error handlers (#858)

--- tock/tock/views.py
+++ tock/tock/views.py
@@ -32,19 +32,3 @@ def logout(request):
         )
     else:
         return render(request, 'logout.html')
-
-def handler400(request, exception):
-    return render(request, '400.html', {})
-
-def handler403(request, exception):
-    response = render(request, '403.html', {'exception': exception})
-    response.status_code = 403
-    return response
-
-def handler404(request, exception):
-    response = render(request, '404.html', {})
-    response.status_code = 404
-    return response
-
-def handler500(request, exception=None):
-    return render(request, '500.html', {})
