00865ba2b76ad622e3297dce2fa957a0cdb9978d
Mat Caughron <mathew.caughron@gsa.gov>
2020-04-23 11:51:38 -0500

    adding DEFAULT_ORG_ID
    default for Organization set with constant

--- tock/employees/models.py
+++ tock/employees/models.py
@@ -1,6 +1,9 @@
 import datetime
 from decimal import Decimal
 
+# DEFAULT_ORG_ID to correspond to '18F'
+DEFAULT_ORG_ID = 1
+
 from django.apps import apps
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator
@@ -96,7 +99,7 @@ class UserData(models.Model):
         default=False,
         verbose_name='Is alternative work schedule eligible'
     )
-    organization = models.ForeignKey(Organization, blank=True, null=True, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, default=DEFAULT_ORG_ID, on_delete=models.CASCADE)
     unit = models.ForeignKey(Unit, blank=True, null=True, on_delete=models.CASCADE,
         verbose_name="Business Unit",
         help_text="The business unit within the organization in which this person sits."
