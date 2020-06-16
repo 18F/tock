38c95761fe20cc6cf8b609a8d5a62e857ecdc7a2
cryptofilegsa <mathew.caughron@gsa.gov>
2020-06-16 13:37:06 -0500

    two new columns for percent billability and org

--- tock/hours/admin.py
+++ tock/hours/admin.py
@@ -84,7 +84,7 @@ class TimecardAdminForm(ModelForm):
 
 class TimecardAdmin(admin.ModelAdmin):
     inlines = (TimecardObjectInline,)
-    list_display = ('user', 'reporting_period', 'submitted')
+    list_display = ('user', 'reporting_period', 'billable_expectation', 'organization', 'submitted')
     list_filter = (ReportingPeriodListFilter, 'reporting_period',)
     search_fields = ['user__username', 'reporting_period__start_date', 'reporting_period__end_date',]
     form = TimecardAdminForm
