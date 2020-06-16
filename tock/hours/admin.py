3e3ad477e09d0298ca01aa3dc28e20c916632c6a
cryptofilegsa <mathew.caughron@gsa.gov>
2020-06-16 13:52:09 -0500

    billable percent now editable

--- tock/hours/admin.py
+++ tock/hours/admin.py
@@ -85,7 +85,7 @@ class TimecardAdminForm(ModelForm):
 class TimecardAdmin(admin.ModelAdmin):
     inlines = (TimecardObjectInline,)
     list_display = ('user', 'reporting_period', 'billable_expectation', 'organization', 'submitted')
-    list_editable = ('organization',)
+    list_editable = ('organization', 'billable_expectation')
     list_filter = (ReportingPeriodListFilter, 'reporting_period',)
     search_fields = ['user__username', 'reporting_period__start_date', 'reporting_period__end_date',]
     form = TimecardAdminForm
