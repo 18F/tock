2525163f337e3048417d49f4cd29dbb1d48a5441
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2021-03-09 13:08:49 -0600

    Add basic project engagement view

--- tock/projects/urls.py
+++ tock/projects/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ProjectListView, ProjectView
+from .views import ProjectListView, ProjectView, ProjectEngagementView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='ProjectListView'),
     path('<int:pk>/', ProjectView.as_view(), name='ProjectView'),
+    path('engagement/', ProjectEngagementView.as_view(),
+        name="ProjectEngagementView"),
 ]
