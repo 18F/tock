03f902f641a122b2d9fe5737223133933af4218f
Amy Mok <amy.mok@gsa.gov>
2021-05-10 15:41:56 -0700

    Implement management command to deactivate stale users

---
+++ tock/tock/management/commands/deactivate_stale_users.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta, timezone
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+@transaction.atomic
+class Command(BaseCommand):
+    help = 'Deactivate user when they have not logged in for a set amount of time'
+
+    def add_arguments(self, parser):
+        parser.add_argument('days_not_logged_in', nargs='?', type=int, default=90)
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        now = datetime.now(timezone.utc)
+
+        past_time = now - timedelta(days=options['days_not_logged_in'])
+        user_list = User.objects.filter(last_login__lte=past_time, is_active=True)
+        user_count = 0
+        token_count = 0
+        for user in user_list:
+          user.is_active = False
+          user.save()
+          user_count += 1
+
+          token = Token.objects.filter(user=user)
+          if token:
+            token.delete()
+            token_count += 1
+
+        self.stdout.write(self.style.SUCCESS(f'Found and deactivated {user_count} users who did not logged in for {options["days_not_logged_in"]} days.'))
+        self.stdout.write(self.style.SUCCESS(f'Found and removed {token_count} tokens with associated users.'))
---
+++ tock/tock/management/commands/tests/test_deactivate_stale_users.py
@@ -0,0 +1,125 @@
+from datetime import datetime, timedelta, timezone
+from io import StringIO
+
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+
+class DeactivateStaleUsersTest(TestCase):
+    DEFAULT_STALE_DAYS = 90
+
+    def create_user(self, username, date_joined, last_login, is_active, need_token):
+        user = User.objects.create_user(username, '')
+        user.date_joined = date_joined
+        user.last_login = last_login
+        user.is_active = is_active
+        user.save()
+
+        if need_token:
+            Token.objects.create(user=user)
+
+    def assert_stdout(self, user_deactivated_count, token_removed_count, stdout):
+        self.assertIn(f'deactivated {user_deactivated_count} users', stdout)
+        self.assertIn(f'{self.DEFAULT_STALE_DAYS} days', stdout)
+        self.assertIn(f'removed {token_removed_count} tokens', stdout)
+
+
+    def test_deactivate_stale_user__one_active_user_with_no_token(self):
+        now = datetime.now(timezone.utc)
+        username = 'Jane.Doe'
+        self.create_user(username,
+                         now - timedelta(days=self.DEFAULT_STALE_DAYS + 120),
+                         now - timedelta(days=self.DEFAULT_STALE_DAYS),
+                         True,
+                         False)
+
+        before_user_count = User.objects.count()
+        before_token_count = Token.objects.count()
+
+        out = StringIO()
+        call_command('deactivate_stale_users', stdout=out)
+        self.assert_stdout(1, 0, out.getvalue())
+
+        self.assertEqual(before_user_count, User.objects.count())
+        self.assertEqual(before_token_count, Token.objects.count())
+
+        updated_user = User.objects.get(username=username)
+        self.assertFalse(updated_user.is_active)
+
+        with self.assertRaises(Token.DoesNotExist):
+            Token.objects.get(user=updated_user)
+
+    def test_deactivate_stale_user__one_active_user_with_token(self):
+        now = datetime.now(timezone.utc)
+        username = 'John.Smith'
+        self.create_user(username,
+                         now - timedelta(days=self.DEFAULT_STALE_DAYS + 120),
+                         now - timedelta(days=self.DEFAULT_STALE_DAYS),
+                         True,
+                         True)
+
+        before_user_count = User.objects.count()
+        before_token_count = Token.objects.count()
+
+        out = StringIO()
+        call_command('deactivate_stale_users', stdout=out)
+        self.assert_stdout(1, 1, out.getvalue())
+
+        self.assertEqual(before_user_count, User.objects.count())
+        self.assertEqual(before_token_count - 1, Token.objects.count())
+
+        updated_user = User.objects.get(username=username)
+        self.assertFalse(updated_user.is_active)
+
+        with self.assertRaises(Token.DoesNotExist):
+            Token.objects.get(user=updated_user)
+
+    def test_deactivate_stale_user__nonactive_user(self):
+        now = datetime.now(timezone.utc)
+        username = 'Long.Time'
+        self.create_user(username,
+                         now - timedelta(days=self.DEFAULT_STALE_DAYS + 120),
+                         now - timedelta(days=self.DEFAULT_STALE_DAYS),
+                         False,
+                         False)
+
+        before_user_count = User.objects.count()
+        before_token_count = Token.objects.count()
+
+        out = StringIO()
+        call_command('deactivate_stale_users', stdout=out)
+        self.assert_stdout(0, 0, out.getvalue())
+
+        self.assertEqual(before_user_count, User.objects.count())
+        self.assertEqual(before_token_count, Token.objects.count())
+
+        updated_user = User.objects.get(username=username)
+        self.assertFalse(updated_user.is_active)
+
+        with self.assertRaises(Token.DoesNotExist):
+            Token.objects.get(user=updated_user)
+
+    def test_deactivate_stale_user__one_nonstale_active_user_with_token(self):
+        now = datetime.now(timezone.utc)
+        username = 'Mary.Wright'
+        self.create_user(username,
+                         now - timedelta(days=self.DEFAULT_STALE_DAYS + 120),
+                         now,
+                         True,
+                         True)
+
+        before_user_count = User.objects.count()
+        before_token_count = Token.objects.count()
+
+        out = StringIO()
+        call_command('deactivate_stale_users', stdout=out)
+        self.assert_stdout(0, 0, out.getvalue())
+
+        self.assertEqual(before_user_count, User.objects.count())
+        self.assertEqual(before_token_count, Token.objects.count())
+
+        updated_user = User.objects.get(username=username)
+        self.assertTrue(updated_user.is_active)
+
+        self.assertTrue(Token.objects.get(user=updated_user))
