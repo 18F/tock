687a5368954e4614ad0714ba1ea570ee1602f569
Neil Martinsen-Burrell <neil.martinsen-burrell@gsa.gov>
2021-09-30 12:11:33 -0500

    Split migration to fix deploy to staging

--- tock/hours/migrations/0063_small_allocation.py
+++ tock/hours/migrations/0063_small_allocation.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='timecardobject',
             name='project_allocation',
-            field=models.DecimalField(choices=[(0, '---'), (1.0, '100%'), (0.5, '50%'), (0.25, '25%'), (0.125, '12.5%')], decimal_places=3, default=0, max_digits=6),
+            field=models.DecimalField(choices=[(0, '---'), (1.0, '100%'), (0.5, '50%'), (0.25, '25%'), (0.125, '12.5%')], decimal_places=2, default=0, max_digits=5),
         ),
     ]
